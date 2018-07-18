eb69d6c9b58cd206a1bf90984d30d93e2bc1c971
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2018-07-18 18:49:14 +0200

    Added EB database config and requirements files

--- API/MyToDo/settings.py
+++ API/MyToDo/settings.py
@@ -163,16 +163,28 @@ WSGI_APPLICATION = 'MyToDo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'localtask',
-        'USER': 'localtask',
-        'PASSWORD': 'localtask',
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'localtask',
+            'USER': 'localtask',
+            'PASSWORD': 'localtask',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Cache
 
@@ -230,4 +242,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
