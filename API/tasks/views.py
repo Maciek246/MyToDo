ec4c52e238a6ec97f0ffc60157ec2c53c56c2f8c
Maciej Matuszczak <maciej.matuszczak96@gmail.com>
2018-07-24 12:56:44 +0200

    Move requirements.txt to API dir, add command to create users and config file for aws

--- API/tasks/views.py
+++ API/tasks/views.py
@@ -1,5 +1,10 @@
+from django.db.models import Count
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Task
 from .serializers import TaskSerializer, TaskCreateSerializer
@@ -23,3 +28,11 @@ class TaskViewSet(ModelViewSet):
             return TaskCreateSerializer
         else:
             return TaskSerializer
+
+    @action(methods=['GET'], detail=False)
+    def dates(self, request, *args, **kwargs):
+        qs = self.get_queryset()\
+            .extra(select={'start': 'Date(start)'})\
+            .values('start')\
+            .annotate(tasks=Count('start'))
+        return Response(qs, 200)
---
+++ API/users/management/commands/create_user.py
@@ -0,0 +1,23 @@
+import argparse
+
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if options['is_superuser'] and options['is_superuser'] == 1:
+            if not User.objects.filter(username=options['username']).exists():
+                User.objects.create_superuser(username=options['username'], password=options['password'], email=options['email'])
+        else:
+            if not User.objects.filter(username=options['username']).exists():
+                User.objects.create_user(username=options['username'], password=options['password'], email=options['email'])
+			
+    def add_arguments(self, parser):
+        parser.add_argument('username', type=str)
+        parser.add_argument('password', type=str)
+        parser.add_argument('email', type=str)
+        parser.add_argument('is_superuser', type=int, nargs='?', default=0)
